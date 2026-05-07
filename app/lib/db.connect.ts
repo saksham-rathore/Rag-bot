402bc2976ab2d24f44f219d23e33b04ea92fb3cf
saksham762007 <samrathore07062007@gmail.com>
2026-05-07 18:52:53 +0530

    feat: implement MongoDB connection utility using mongoose

--- app/lib/db.connect.ts
+++ app/lib/db.connect.ts
@@ -1,7 +1,29 @@
-import { Pinecone } from '@pinecone-database/pinecone';
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const pc = new Pinecone({
-  apiKey: process.env.PINECONE_API_KEY
-});
-const index = pc.index('quickstart');
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if(connection.isConnected) {
+        console.log("Already Connected")
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGO_DB_URI || "", {})
+        
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("DB is connected")
+    }catch (error) {
+        console.log("Database connection is failed", error)
+
+        process.exit(1)
+    }
+}
+
+export default dbConnect;
