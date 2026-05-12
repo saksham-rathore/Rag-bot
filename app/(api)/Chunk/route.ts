369ce293ba1d9e87510f17ffbbed2d1573a2b220
saksham762007 <samrathore07062007@gmail.com>
2026-05-12 18:26:49 +0530

    feat: implement document chunking API route and PDF parsing utilities with OpenAI integration

--- app/(api)/Chunk/route.ts
+++ app/(api)/Chunk/route.ts
@@ -6,6 +6,10 @@ import { storeDocumentsInQdrant } from "@/app/lib/qdrant";
 import { splitText } from "@/app/lib/splitter";
 import { NextResponse } from "next/server";
 import { fileURLToPath } from "url";
+import type { Document } from "@langchain/core/documents";
+import type { Embeddings } from "@langchain/core/embeddings";
+import { OpenAIEmbeddings } from "@langchain/openai";
+
 
 export async function POST(req: Request) {
     try {
@@ -17,7 +21,13 @@ export async function POST(req: Request) {
             return NextResponse.json({error: "File Url missing"}, {status: 400});
         }
 
-        
+        const text = await loadPDF(fileUrl);
+
+        const chunks = await splitText(text);
+
+        const vector = await embeddings(chunks);
+
+        await storeDocumentsInQdrant(docs, embeddings)
 
     } catch (error) {
         console.error(error);
