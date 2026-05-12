100502efb6daaa2d83a7d0dcb0427ca25422bdf7
saksham762007 <samrathore07062007@gmail.com>
2026-05-12 18:27:55 +0530

    feat: implement POST route to process PDFs and store document chunks in Qdrant

--- app/(api)/Chunk/route.ts
+++ app/(api)/Chunk/route.ts
@@ -1,14 +1,9 @@
 import dbConnect from "@/app/lib/db.connect";
 import embeddings from "@/app/lib/embeddings";
 import { loadPDF } from "@/app/lib/pdf-parse";
-import openai from "@/app/lib/openai";
 import { storeDocumentsInQdrant } from "@/app/lib/qdrant";
 import { splitText } from "@/app/lib/splitter";
 import { NextResponse } from "next/server";
-import { fileURLToPath } from "url";
-import type { Document } from "@langchain/core/documents";
-import type { Embeddings } from "@langchain/core/embeddings";
-import { OpenAIEmbeddings } from "@langchain/openai";
 
 
 export async function POST(req: Request) {
@@ -23,11 +18,11 @@ export async function POST(req: Request) {
 
         const text = await loadPDF(fileUrl);
 
-        const chunks = await splitText(text);
+        const docs = await splitText(text);
 
-        const vector = await embeddings(chunks);
+        await storeDocumentsInQdrant(docs, embeddings);
 
-        await storeDocumentsInQdrant(docs, embeddings)
+        return NextResponse.json({ message: "Success" }, { status: 200 });
 
     } catch (error) {
         console.error(error);
