2dcaa007efcb399e78e0e18f1bf9a7909f215663
saksham762007 <samrathore07062007@gmail.com>
2026-05-14 14:17:57 +0530

    feat: implement message worker to process PDF files and upsert embeddings to Qdrant

--- app/lib/Worker/message-worker.ts
+++ app/lib/Worker/message-worker.ts
@@ -3,6 +3,8 @@ import { redisConnection } from "../redis";
 import { DirectoryLoader } from "@langchain/classic/document_loaders/fs/directory";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { OpenAIEmbeddings } from "@langchain/openai";
 import axios from "axios";
 
 const worker = new Worker(
@@ -23,10 +25,21 @@ const worker = new Worker(
       chunkSize: 100,
       chunkOverlap: 0,
     });
-    const texts = splitter.splitText(docs);
+    const texts = await splitter.splitDocuments(docs);
 
     // 3. Create embeddings
+    const embeddings = new OpenAIEmbeddings();
+    
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(
+      embeddings,
+      {
+        url: process.env.QDRANT_URL,
+        collectionName: "langchainjs-testing",
+      },
+    );
+
     // 4. Store in DB
+    await vectorStore.addDocuments(texts);
 
     await new Promise((res) => setTimeout(res, 3000));
 
