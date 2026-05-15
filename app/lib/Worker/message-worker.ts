043b762f3fdea0df1192344a5a5d7d3c84595316
saksham762007 <samrathore07062007@gmail.com>
2026-05-15 21:07:35 +0530

    feat: add BullMQ worker to process PDF downloads, chunking, and Qdrant indexing

--- app/lib/Worker/message-worker.ts
+++ app/lib/Worker/message-worker.ts
@@ -14,16 +14,20 @@ const worker = new Worker(
     console.log("Data:", job.data);
 
     // 1. Download PDF
-    const response = await axios.get(job.data.fileUrl, { responseType: 'arraybuffer' });
-    const blob = new Blob([response.data], { type: 'application/pdf' });
-    console.log("Step 1: Downloading PDF");
-    
+    console.log("Step 1: Starting download");
+    const response = await axios.get(job.data.fileUrl, {
+      responseType: "arraybuffer",
+    });
+    console.log("Step 1 DONE");
+
+    console.log("Step 2: Loading PDF");
+    const blob = new Blob([response.data], { type: "application/pdf" });
     const loader = new PDFLoader(blob);
     const docs = await loader.load();
-    console.log("Step 2: Loading PDF");
+    console.log("Step 2 DONE");
 
     // Attach documentId to metadata
-    const docsWithMetadata = docs.map(doc => {
+    const docsWithMetadata = docs.map((doc) => {
       doc.metadata = { ...doc.metadata, documentId: job.data.documentId };
       return doc;
     });
@@ -33,24 +37,22 @@ const worker = new Worker(
       chunkSize: 100,
       chunkOverlap: 0,
     });
-    const texts = await splitter.splitDocuments(docsWithMetadata);
     console.log("Step 3: Chunking");
+    const texts = await splitter.splitDocuments(docsWithMetadata);
+    console.log("Step 3 DONE");
 
     // 3. Create embeddings
     const embeddings = new OpenAIEmbeddings();
     console.log("Step 4: Embeddings");
-    
+
     // 3 & 4. Create embeddings and store in DB
-    await QdrantVectorStore.fromDocuments(
-      texts,
-      embeddings,
-      {
-        url: process.env.QDRANT_ENDPOINT_KEY,
-        apiKey: process.env.QDRANT_API_KEY,
-        collectionName: "langchainjs-testing",
-      },
-    );
-    console.log("Step 5: Upload to Qdrant");
+    console.log("Step 4: Embedding + Qdrant");
+    await QdrantVectorStore.fromDocuments(texts, embeddings, {
+      url: process.env.QDRANT_ENDPOINT_KEY,
+      apiKey: process.env.QDRANT_API_KEY,
+      collectionName: "langchainjs-testing",
+    });
+    console.log("Step 4 DONE");
 
     console.log("Job completed");
   },
@@ -64,7 +66,6 @@ const worker = new Worker(
   },
 );
 
-
 worker.on("completed", (job) => {
   console.log(`Job ${job.id} completed`);
 });
@@ -73,5 +74,5 @@ worker.on("failed", (job, err) => {
   console.log(`Job ${job?.id} failed:`, err);
 });
 
-console.log("Worker started!")
-console.log("Worker is listening for jobs...")
+console.log("Worker started!");
+console.log("Worker is listening for jobs...");
